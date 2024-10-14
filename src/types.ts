92233f67abb4c4eb9b92f1b5dac469f171480152
lukas.hartmann <lukas.hartmann@adabay.rocks>
2024-10-14 15:40:41 +0200

    chore: added optional access field to options

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,31 @@
 import { Config } from "payload/config";
-import { Block, CollectionConfig } from "payload/dist/exports/types";
+import { Block, CollectionConfig, GlobalConfig } from "payload/dist/exports/types";
+
+type BaseOptions = {
+	access?: CollectionConfig['access'];
+};
 
 export type SimpleWebsitePluginOptions = {
 	collections: {
 		pages: {
 			blocks: Block[];
-		};
+		} & BaseOptions;
 		media?: {
 			directory?: string;
-			url?: string
-		}
+			url?: string,
+		} & BaseOptions;
 	};
+	globals: {
+		routingSettings: BaseOptions
+	}
 };
 
 export type CollectionFactory = (
 	config: Config,
 	opts?: SimpleWebsitePluginOptions
 ) => CollectionConfig;
+
+export type GlobalFactory = (
+	config: Config,
+	opts?: SimpleWebsitePluginOptions
+) => GlobalConfig;
